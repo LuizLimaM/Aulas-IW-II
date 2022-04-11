71c322492e408ad132fbed569a74c64c8716901d
Luiz Henrique da Silva Lima <79856769+LuizLimaM@users.noreply.github.com>
2022-04-11 11:00:45 -0300

    Atividades de IW-II

---
+++ aula_07_03_2022/README.md
@@ -0,0 +1,2 @@
+# Atividade-IW-II
+
---
+++ aula_11_04_2022/Interação/clicar.js
@@ -0,0 +1,18 @@
+function clicar() {
+    var area = document.getElementById('area')
+    area.innerHTML = 'Clicou!'
+    area.style.background = "rgb(40, 20, 128)"
+
+}
+
+function entrar() {
+    var area = document.getElementById('area')
+    area.innerHTML = 'Olá'
+
+}
+
+function sair() {
+    var area = document.getElementById('area')
+    area.innerHTML = 'Adios'
+    area.style.background = "rgb(25, 12, 82)"
+}
